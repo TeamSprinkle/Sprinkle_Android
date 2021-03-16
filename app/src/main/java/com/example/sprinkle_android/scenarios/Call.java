0b8044d22eb5757706b6f8cfe1e123e208b5bde0
Kyeongwon <aelleek@gmail.com>
2021-03-17 00:56:17 +0900

    Update requestCommand method and scenario classes

--- app/src/main/java/com/example/sprinkle_android/scenarios/Call.java
+++ app/src/main/java/com/example/sprinkle_android/scenarios/Call.java
@@ -1,12 +1,24 @@
 package com.example.sprinkle_android.scenarios;
 
+import android.content.Context;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class Call extends Scenario{
 
     private String intent;
+    private ArrayList<String> entity;
+    private ArrayList<String> entityAnswer;
 
     public Call()
     {
         this.intent = "call";
+        this.entity.add("target");
+
+        this.entityAnswer.add("누구에게 전화 걸까요?");
     }
 
     public String getIntent()
@@ -14,12 +26,23 @@ public class Call extends Scenario{
         return this.intent;
     }
 
-    public void runScenario(String data)
+    public void runScenario(Context context, String answer)
     {
-        ;
+        JSONObject data = null;
+        String target = "";
+
+        try {
+            data = new JSONObject(answer);
+            target = (String) data.get("target");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
-
-
-
+    public String getRequireAnswer(String entity)
+    {
+        return "";
+    }
 }
