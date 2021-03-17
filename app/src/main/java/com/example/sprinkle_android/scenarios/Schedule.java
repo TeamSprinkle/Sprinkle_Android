9a6f94e7f1c576eb866f908132ca0ecda7e439d7
Kyeongwon <aelleek@gmail.com>
2021-03-17 15:49:59 +0900

    Update Schedule class Constructor

--- app/src/main/java/com/example/sprinkle_android/scenarios/Schedule.java
+++ app/src/main/java/com/example/sprinkle_android/scenarios/Schedule.java
@@ -2,13 +2,22 @@ package com.example.sprinkle_android.scenarios;
 
 import android.content.Context;
 
+import java.util.ArrayList;
+
 public class Schedule extends Scenario{
 
     private String intent;
+    private ArrayList<String> entity;
+    private ArrayList<String> entityAnswer;
 
     public Schedule()
     {
         this.intent = "schedule";
+
+        this.entity.add("Date");
+        this.entity.add("Subject");
+        this.entity.add("Time");
+        this.entity.add("Action");
     }
 
     public String getIntent()
